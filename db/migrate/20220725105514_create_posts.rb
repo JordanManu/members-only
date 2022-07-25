4c1517ecb52b6d88ccef3ecc754dfa3182daca21
Jordan <jordanmanu@live.com.au>
2022-07-25 13:34:22 +0100

    Added User model and controller

--- db/migrate/20220725105514_create_posts.rb
+++ db/migrate/20220725105514_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
+      t.string :username
+      t.string :email
+      t.string :password
 
       t.timestamps
     end
