3f33441ae19c1663a08f77ebd4fd7f806d90bef8
Jordan <jordanmanu@live.com.au>
2022-07-26 15:37:32 +0100

    User sign out now working, had to disable turbo drive on log out button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,57 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  def new 
-    @post = Post.new
+
+  def index
+    @posts = Post.all
   end
 
-end
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new 
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to root_path, notice: "Post was successfully created." }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy 
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:tweet)
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users do
+    get '/sign_out' => 'devise/sessions#destroy'
+    get '/log_in' => 'devise/sessions#new'
+    get '/log_out' => 'devise/sessions#destroy'
+    get '/sign_up' => 'devise/registrations#new'
+    get '/edit_profile' => 'devise/registrations#edit'
+ end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "posts#index"
 
-  resources :posts, only: [:new, :create, :index]
+  resources :posts
+
 end
